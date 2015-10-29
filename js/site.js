e7ca5e8278d5a375c9ecab3984d429de325271ad
diogo <diogo.j.menezes@gmail.com>
2015-10-29 13:49:19 -0200

    login atual

--- js/site.js
+++ js/site.js
@@ -5,5 +5,29 @@ $("a#efetuarCadastro").click(function(){
 	$("#cadastro").css('display', 'block');
 });
 $(document).ready(function(){
-	
+	$("a#linkperdilogin").click(function(){
+		event.preventDefault();
+		$("form#login").css('display', 'none');
+		$("form#perdilogin").css('display', 'block');
+		$("a#linkperdilogin").css('display','none');
+		$("#linkvoltarlogin").css('display','inline-block');
+		$("#linkmudarsenha").css('display','none');
+	});	
+	$("#linkvoltarlogin").click(function(){
+		event.preventDefault();
+		$("a#linkperdilogin").css('display','inline-block');
+		$("form#login").css('display', 'block');
+		$("form#perdilogin").css('display', 'none');
+		$("#linkmudarsenha").css('display','inline-block');
+		$("form#mudarsenha").css('display', 'none');
+		$("#linkvoltarlogin").css('display','none');
+	});
+	$("#linkmudarsenha").click(function(){
+		event.preventDefault();
+		$("form#login").css('display', 'none');
+		$("form#mudarsenha").css('display', 'block');
+		$("a#linkperdilogin").css('display','none');
+		$("#linkvoltarlogin").css('display','inline-block');
+		$("#linkmudarsenha").css('display','none');
+	});
 });
